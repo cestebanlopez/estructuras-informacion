4ad1460b6e38b18217d2fad56e7a98d0e3d744d7
CAMILO <CAMILO@DESKTOP-9LUHGSA>
2021-10-08 14:56:37 -0500

    Taller4

---
+++ Taller4/src/main/java/co/edu/ucundinamarca/taller3/Dulces.java
@@ -0,0 +1,39 @@
+package co.edu.ucundinamarca.taller3;
+
+import java.util.Iterator;
+
+public class Dulces {
+    
+    public static void main(String[] args) {
+        
+        Dulces arrayList[]=new Dulces[10]; 
+        arrayList[0]=new Dulces("Pipiolo");
+        arrayList[1]=new Dulces("Detodito");
+        arrayList[2]=new Dulces("Chocobreak");
+        arrayList[3]=new Dulces("Bombombum");
+        arrayList[4]=new Dulces("Frunas");
+        arrayList[5]=new Dulces("Galletas");
+        arrayList[6]=new Dulces("Chocolatina");
+        arrayList[7]=new Dulces("Tostacos");
+        arrayList[8]=new Dulces("Gomitas");
+        arrayList[9]=new Dulces("Choco Ramo");
+        
+     
+    }
+
+   public Dulces (String nombre){
+       this.nombre=nombre;
+    }
+    
+   public String getNombre() {
+        return nombre;
+    }
+
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+     
+  private String nombre;
+}
+
---
+++ Taller4/src/main/java/co/edu/ucundinamarca/taller3/Main.java
@@ -0,0 +1,26 @@
+package co.edu.ucundinamarca.taller3;
+/**
+ *
+ * @author CAMILO
+ */
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        Dulces arrayList[]=new Dulces[10]; 
+        arrayList[0]=new Dulces("Pipiolo");
+        arrayList[1]=new Dulces("Detodito");
+        arrayList[2]=new Dulces("Chocobreak");
+        arrayList[3]=new Dulces("Bombombum");
+        arrayList[4]=new Dulces("Frunas");
+        arrayList[5]=new Dulces("Galletas");
+        arrayList[6]=new Dulces("Chocolatina");
+        arrayList[7]=new Dulces("Tostacos");
+        arrayList[8]=new Dulces("Gomitas");
+        arrayList[9]=new Dulces("Choco Ramo");
+        
+        
+    }
+   
+}
