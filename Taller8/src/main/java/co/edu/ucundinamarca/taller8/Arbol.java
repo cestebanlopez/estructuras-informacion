084d2702de74d265c9bde66d66d0d84ac0d9ea28
CAMILO <CAMILO@DESKTOP-9LUHGSA>
2021-11-12 15:26:41 -0500

    Taller8

---
+++ Taller8/src/main/java/co/edu/ucundinamarca/taller8/Arbol.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.ucundinamarca.taller8;
+
+/**
+ *
+ * @author CAMILO
+ */
+public class Arbol {
+    
+}
